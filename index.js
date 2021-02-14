e0af6d937603d5a6e35c7ca7f473e0177bfdca37
ROEL2407 <roel24komen@gmail.com>
2021-02-14 14:38:25 +0100

    Handlebars templates

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home', {})
+});
+
+app.get('/about', (req, res) => {
+  res.send('Hello about!')
+});
+
+app.get('/login', (req, res) => {
+  res.send('Hello login!')
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry this page doesn't excist, try another one")
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
+
+app.engine('handlebars', exphbs());
+app.set("view engine", 'handlebars');
