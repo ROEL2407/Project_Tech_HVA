0c10edef74343346986cdb623a7255e37f0ce7c0
ROEL2407 <roel24komen@gmail.com>
2021-02-21 15:30:13 +0100

    html en css feature

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
 const port = 3000;
+app.use(express.static('static'));
+
+const movies = [
+  {title: "The Hulk", genre: "Action", jaartal: 1980},
+  {title: "film3", genre: "comedie", jaartal: 1990},
+  {title: "film4", genre: "banaan", jaartal: 2020},
+]
 
 app.get('/', (req, res) => {
   res.render('home', {})
@@ -13,6 +20,11 @@ app.get('/about', (req, res) => {
 
 app.get('/login', (req, res) => {
   res.send('Hello login!')
+});
+
+//voorbeeld dynamic code v
+app.get('/movies', (req, res) => {
+  res.render('ListOfMovies', {title: "List of movies", movies})
 });
 
 app.use(function (req, res, next) {
