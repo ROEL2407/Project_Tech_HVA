68bd8b80d2b4f9664e81f148ea1867786247cdb7
ROEL2407 <roel24komen@gmail.com>
2021-03-05 13:29:18 +0100

    removed lodash and camelcase

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const {pick} = require('lodash');
 const app = express();
 const port = 3000;
 const dotenv = require('dotenv').config();
